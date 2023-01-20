913b54036c56ed0aa911e01d1897938548950f28
jaggerabney <jagger.abney@gmail.com>
2023-01-20 15:50:59 -0800

    server now responds with html data

--- project-1/app.js
+++ project-1/app.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(
+    "<html><head><title>My First Page</title></head><body><h1>Hello from my Node.js server!</h1></body></html>"
+  );
+  res.end();
+
+  process.exit();
 });
 
 server.listen(3000);
