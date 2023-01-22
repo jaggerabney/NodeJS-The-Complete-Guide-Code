4b29d0acfe0943ecb7964ec828a95707aea70bec
jaggerabney <jagger.abney@gmail.com>
2023-01-22 13:26:10 -0800

    added two routes: "/" and "/users"

--- project-3/app.js
+++ project-3/app.js
@@ -14,4 +14,16 @@ app.use((req, res, next) => {
   next();
 });
 
+// then actually handle routes and return HTML
+
+app.use("/users", (req, res, next) => {
+  res.send(
+    '<h1>Users Page</h1><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul><a href="/">Go to Home page</a>'
+  );
+});
+
+app.use("/", (req, res, next) => {
+  res.send('<h1>Home Page</h1><a href="/users">Go to Users page</a>');
+});
+
 app.listen(3000);
