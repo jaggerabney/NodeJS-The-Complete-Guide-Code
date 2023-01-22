9fc15827464188f8742e7a7249391eec1d5b8350
jaggerabney <jagger.abney@gmail.com>
2023-01-22 12:52:58 -0800

    added express.js

--- project-1/app.js
+++ project-1/app.js
@@ -1,7 +1,15 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-console.log(routes.text);
-const server = http.createServer(routes.handler);
+const app = express();
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
