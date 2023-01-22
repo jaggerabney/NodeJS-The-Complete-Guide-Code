cc37a0fbdabb6e54b8c2fccdaec89b2cdc7031b6
jaggerabney <jagger.abney@gmail.com>
2023-01-22 12:59:31 -0800

    added some middleware and utilized express.listen

--- project-1/app.js
+++ project-1/app.js
@@ -8,8 +8,7 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
   console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
