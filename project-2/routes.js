4480a9eaf162e3b4d6f66ee4f760abec58f66957
jaggerabney <jagger.abney@gmail.com>
2023-01-22 00:11:01 -0800

    added "/create-user" route

--- project-2/routes.js
+++ project-2/routes.js
@@ -1,9 +1,9 @@
 function requestHandler(req, res) {
-  const { url } = req;
+  const { url, method } = req;
 
   if (url === "/") {
     res.write(
-      '<html><head><title>Greetings!</title></head><body><h1>Greetings, user!</h1><input type="text" action="/create-user" method="POST"><button type="submit">Send</button><br><br><a href="/users">Go to Users page</a></body></html>'
+      '<html><head><title>Greetings!</title></head><body><h1>Greetings, user!</h1><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form><a href="/users">Go to Users page</a></body></html>'
     );
 
     return res.end();
@@ -15,6 +15,26 @@ function requestHandler(req, res) {
     );
 
     return res.end();
+  }
+
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const username = parsedBody.split("=")[1];
+
+      console.log(username);
+      res.writeHead(302, {
+        Location: "/",
+      });
+
+      return res.end();
+    });
   }
 }
 
