1b2a5d92823316b235888b8f77f68488cbf0f522
jaggerabney <jagger.abney@gmail.com>
2023-01-22 13:13:34 -0800

    app now returns html

--- project-1/app.js
+++ project-1/app.js
@@ -2,12 +2,17 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the middleware!");
+
+app.use("/", (req, res, next) => {
+  console.log("In middleware!");
   next();
 });
-app.use((req, res, next) => {
-  console.log("In another middleware!");
+
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>Add Product Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
