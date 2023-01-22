299801eb3cf19bf1105f812041a6f6f20471241d
jaggerabney <jagger.abney@gmail.com>
2023-01-22 13:21:49 -0800

    added two middleware functions

--- project-3/app.js
+++ project-3/app.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const app = express();
 
+// funnel requests through two middleware functions
+
+app.use((req, res, next) => {
+  console.log("In middleware function #1!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In middleware function #2!");
+  next();
+});
+
 app.listen(3000);
