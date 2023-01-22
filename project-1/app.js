29cc5b6a3fe45b8161424304bb1f4fb963e8f22b
jaggerabney <jagger.abney@gmail.com>
2023-01-22 13:36:51 -0800

    added form to "/add-product" (and body-parser)

--- project-1/app.js
+++ project-1/app.js
@@ -1,15 +1,24 @@
-const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded());
+
 app.use("/", (req, res, next) => {
-  console.log("In middleware!");
+  // console.log("In middleware!");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Add Product Page</h1>");
+  res.send(
+    '<h1>Add Product Page</h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
