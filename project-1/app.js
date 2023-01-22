61e0b7eb0cac5c53bbc97c136a7a3b0c9d14f1ef
jaggerabney <jagger.abney@gmail.com>
2023-01-22 13:39:58 -0800

    changed "/product" route to use express.post

--- project-1/app.js
+++ project-1/app.js
@@ -5,18 +5,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded());
 
-app.use("/", (req, res, next) => {
-  // console.log("In middleware!");
-  next();
-});
-
 app.use("/add-product", (req, res, next) => {
   res.send(
     '<h1>Add Product Page</h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>'
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
