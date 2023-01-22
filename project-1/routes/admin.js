6e4c4357a95a7bb02b7b6a13bda645b079d7eac2
jaggerabney <jagger.abney@gmail.com>
2023-01-22 14:17:49 -0800

    added shop and add-product html files

--- project-1/routes/admin.js
+++ project-1/routes/admin.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<h1>Add Product Page</h1><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
