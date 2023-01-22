6c6fa5eddf71140ee0a17c21ae01de144ca6fafe
jaggerabney <jagger.abney@gmail.com>
2023-01-21 22:10:21 -0800

    added routing

--- project-1/app.js
+++ project-1/app.js
@@ -1,15 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const { url, method } = req;
+
+  if (url === "/") {
+    res.write(
+      '<html><head><title>Enter message</title></head><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>'
+    );
+
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "dummy text");
+
+    res.writeHead(302, {
+      Location: "/",
+    });
+
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write(
     "<html><head><title>My First Page</title></head><body><h1>Hello from my Node.js server!</h1></body></html>"
   );
   res.end();
-
-  process.exit();
 });
 
 server.listen(3000);
