fe03fced0dbf9b99077c89c656d0f191e7f94945
jaggerabney <jagger.abney@gmail.com>
2023-01-23 22:08:01 -0800

    added routes for "/" and "/users"

--- project-4/app.js
+++ project-4/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
+const mainRoute = require("./routes/main");
+const usersRoute = require("./routes/users");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(mainRoute);
+app.use(usersRoute);
 
 app.listen(3000);
