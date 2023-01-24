aba944b65d42fa6814311760a334025b13a90f58
jaggerabney <jagger.abney@gmail.com>
2023-01-23 21:09:21 -0800

    added rootDir helper function

--- project-1/routes/admin.js
+++ project-1/routes/admin.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
