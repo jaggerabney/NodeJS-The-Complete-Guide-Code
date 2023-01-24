e251054ea34396f425893e643de39bb909740e2c
jaggerabney <jagger.abney@gmail.com>
2023-01-23 21:47:01 -0800

    extracted styling to main.css and product.css

--- project-1/app.js
+++ project-1/app.js
@@ -8,6 +8,7 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
