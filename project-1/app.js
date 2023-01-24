579ca8d9a93d0363044a430ac4346c8e3ec2fa0e
jaggerabney <jagger.abney@gmail.com>
2023-01-23 21:04:48 -0800

    added 404 page

--- project-1/app.js
+++ project-1/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
@@ -12,7 +13,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found...</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
