1cce5ca02a8b8e7041c75c7970c379a924fbfebd
jaggerabney <jagger.abney@gmail.com>
2023-01-25 13:48:14 -0800

    converted 404.html to pug

--- project-1/app.js
+++ project-1/app.js
@@ -17,7 +17,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
