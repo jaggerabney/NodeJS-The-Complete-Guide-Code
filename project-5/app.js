8887887dd93fde8fc6fe7c9ad48a28aab3190e1d
jaggerabney <jagger.abney@gmail.com>
2023-01-25 17:00:32 -0800

    added route and pug file for "/" route

--- project-5/app.js
+++ project-5/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 
+const mainRoute = require("./routes/main");
+
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(mainRoute);
 
 app.listen(3000);
