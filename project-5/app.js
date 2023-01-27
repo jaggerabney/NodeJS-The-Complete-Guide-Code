d5fd0688514164cfcd46bcf3bd7f292baa015ef8
jaggerabney <jagger.abney@gmail.com>
2023-01-27 00:25:36 -0800

    added styling and main-layout for pages

--- project-5/app.js
+++ project-5/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const mainRoute = require("./routes/main");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "pug");
 app.set("views", "views");
