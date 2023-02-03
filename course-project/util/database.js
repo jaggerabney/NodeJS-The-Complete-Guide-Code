8a1f5828f05dbd82c11dc39501bdc0d1f2e0ddea
jaggerabney <jagger.abney@gmail.com>
2023-02-02 22:29:03 -0800

    installed sequelize

--- course-project/util/database.js
+++ course-project/util/database.js
@@ -1,11 +1,14 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: process.env.DATABASE_PASSWORD,
-});
+const sequelize = new Sequelize(
+  "node-complete",
+  "root",
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
