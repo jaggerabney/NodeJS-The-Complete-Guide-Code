c2753c88165416baf0962a2e28f8043a46248974
jaggerabney <jagger.abney@gmail.com>
2023-02-06 16:37:59 -0800

    deleted cart and cart-item; added addToCart method

--- course-project/models/user.js
+++ course-project/models/user.js
@@ -3,13 +3,31 @@ const { ObjectId } = require("mongodb");
 const db = require("../util/database").getDb;
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, _id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = _id;
   }
 
   save() {
     return db().collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProduct = this.cart.items.findIndex(
+      (element) => element._id === product._id
+    );
+
+    if (cartProduct) {
+      // to be added
+    } else {
+      const updatedCart = { items: [{ ...product, quantity: 1 }] };
+
+      return db()
+        .collection("users")
+        .updateOne({ _id: new ObjectId(_id) }, { $set: { cart: updatedCart } });
+    }
   }
 
   static findById(userId) {
