6653676a623ab1cc49a9ab50be3a2a1bf63cc74a
jaggerabney <jagger.abney@gmail.com>
2023-02-11 23:33:57 -0800

    authentication status is saved as cookie

--- course-project/controllers/auth.js
+++ course-project/controllers/auth.js
@@ -1,13 +1,23 @@
 exports.getLoginPage = function (req, res, next) {
+  // forgive me, father, for this statement is a sin
+  const isLoggedIn =
+    req
+      .get("Cookie")
+      .split(";")
+      .find((cookie) => cookie.split("=")[0].localeCompare("isLoggedIn"))
+      .split("=")[1] === "true";
+
+  console.log(isLoggedIn);
+
   res.render("auth/login", {
     title: "Login",
     path: "/login",
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLoginPage = function (req, res, next) {
-  req.isLoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true");
 
   res.redirect("/");
 };
