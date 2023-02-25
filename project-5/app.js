f2ea5ecfb6e08093f01b877ec42421a3ee238aa1
jaggerabney <jagger.abney@gmail.com>
2023-02-24 21:43:18 -0800

    added middleware to deal with CORS errors

--- project-5/app.js
+++ project-5/app.js
@@ -7,6 +7,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
