8c2df6e4613459314c9abdc15e13a0998be53182
jaggerabney <jagger.abney@gmail.com>
2023-02-28 10:33:18 -0800

    added User model

--- course-project-2/backend/app.js
+++ course-project-2/backend/app.js
@@ -8,6 +8,7 @@ const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -44,6 +45,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
