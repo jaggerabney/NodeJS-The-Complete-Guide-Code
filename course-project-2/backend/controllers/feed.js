70ec91a57d78a3cac57dde10f8fd7e1c3ab5c392
jaggerabney <jagger.abney@gmail.com>
2023-02-28 23:15:41 -0800

    Users now can't change posts that aren't theirs

--- course-project-2/backend/controllers/feed.js
+++ course-project-2/backend/controllers/feed.js
@@ -153,6 +153,13 @@ exports.updatePost = function (req, res, next) {
         throw error;
       }
 
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized!");
+        error.statusCode = 403;
+
+        throw error;
+      }
+
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -180,11 +187,16 @@ exports.deletePost = function (req, res, next) {
 
   Post.findById(postId)
     .then((post) => {
-      // check logged in user
-
       if (!post) {
         const error = new Error("Couldn't find post!");
         error.statusCode = 404;
+
+        throw error;
+      }
+
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized!");
+        error.statusCode = 403;
 
         throw error;
       }
@@ -193,8 +205,12 @@ exports.deletePost = function (req, res, next) {
 
       return Post.findByIdAndRemove(postId);
     })
-    .then((result) => {
-      console.log(result);
+    .then(() => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      user.save();
 
       return res.status(200).json({ message: "Deleted post!" });
     })
