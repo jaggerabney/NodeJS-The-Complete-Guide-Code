5b3a84606c88d3faf230f4f89eefbf608f8458a1
jaggerabney <jagger.abney@gmail.com>
2023-03-01 20:54:38 -0800

    refactored/commented isAuth middleware

--- course-project-2/backend/middleware/isAuth.js
+++ course-project-2/backend/middleware/isAuth.js
@@ -1,35 +1,35 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const { throwCustomError, addStatusCodeTo } = require("../util/error");
+
 module.exports = (req, res, next) => {
+  // Gets auth info from Authorization request header
   const authHeader = req.get("Authorization");
 
+  // Verifies that the auth header is defined
   if (!authHeader) {
-    const error = new Error("No Authorization header!");
-    error.statusCode = 401;
-
-    throw error;
+    throwCustomError("No Authorization header!", 401);
   }
-
-  const token = authHeader.split(" ")[1];
-  let decodedToken;
 
   try {
-    decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    // Tries to get and decode the auth token from the auth header
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+
+    // Verifies that the decoded token is defined; if not, something went wrong,
+    // so an error is thrown
+    if (!decodedToken) {
+      throwCustomError("Authorization failed!", 401);
+    }
+
+    // Assigns the userId field to the request so that future middlewares
+    // can use the logged in user's ID
+    req.userId = decodedToken.userId;
+
+    // Passes the request to the next middleware
+    next();
   } catch (error) {
-    error.statusCode = 500;
-
-    throw error;
+    next(addStatusCodeTo(error));
   }
-
-  if (!decodedToken) {
-    const error = new Error("Authentication failed!");
-    error.statusCode = 401;
-
-    throw error;
-  }
-
-  req.userId = decodedToken.userId;
-
-  next();
 };
