45e3c870fbe605943f81158a41bab50244a1f668
jaggerabney <jagger.abney@gmail.com>
2023-03-02 15:15:24 -0800

    commented route files

--- course-project-2/backend/routes/auth.js
+++ course-project-2/backend/routes/auth.js
@@ -1,13 +1,27 @@
+// Third-party imports
 const express = require("express");
 const { body } = require("express-validator");
 
+// Project imports
 const User = require("../models/user");
 const authController = require("../controllers/auth");
 
+// Creates router
 const router = express.Router();
 
+// Routes
+
+// These routes are *not* protected by the isAuth middleware, as they
+// are meant to be accessed by users that aren't logged in
+
+// PUT -> /signup
 router.put(
   "/signup",
+  // Checks that:
+  // - The email is valid
+  // - The (normalized) email doesn't already belong to an account
+  // - The password is at least five characters long
+  // - The name is not empty
   [
     body("email", "Please enter a valid email!").isEmail(),
     body("email", "An account already exists with this email address.")
@@ -28,7 +42,8 @@ router.put(
   ],
   authController.signup
 );
-
+// POST -> /login
 router.post("/login", authController.login);
 
+// Exports the router object for use in App.js
 module.exports = router;
--- course-project-2/backend/routes/feed.js
+++ course-project-2/backend/routes/feed.js
@@ -1,38 +1,63 @@
+// Third-party imports
 const express = require("express");
-const { check, body } = require("express-validator");
+const { body } = require("express-validator");
 
+// Project imports
 const feedController = require("../controllers/feed");
 const isAuth = require("../middleware/isAuth");
 
+// Creates router
 const router = express.Router();
 
+// GET Routes
+
+// All of the below routes use the isAuth middleware before the controller
+// middleware to verify that the user is currently logged in
+
+// GET -> /posts
 router.get("/posts", isAuth, feedController.getPosts);
+// GET -> /posts/[postId]
 router.get("/post/:postId", isAuth, feedController.getPost);
+// GET -> /status
 router.get("/status", isAuth, feedController.getStatus);
+// POST -> /post
+
+// POST/PUT/DELETE Routes
+
+// Like the GET routes, all of these routes are protected by the isAuth middleware
+
+// POST -> /post
 router.post(
   "/post",
   isAuth,
+  // Checks if the title and content are at least five characters long
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
   feedController.createPost
 );
+// PUT -> /post/[postId]
 router.put(
   "/post/:postId",
   isAuth,
+  // Checks that the title and content are at least five characters long
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
   feedController.updatePost
 );
+// PUT -> /status
 router.put(
   "/status",
   isAuth,
+  // Checks that the status is not empty
   body("status").trim().not().isEmpty(),
   feedController.updateStatus
 );
+// DELETE -> /post/[postId]
 router.delete("/post/:postId", isAuth, feedController.deletePost);
 
+// Exports the router object for use in App.js
 module.exports = router;
