3ee78bd77edbea60d6350158ac9bee5ba205695f
jaggerabney <jagger.abney@gmail.com>
2023-03-03 20:39:20 -0800

    added validation to createUser resolver

--- course-project-2/backend/graphql/resolvers.js
+++ course-project-2/backend/graphql/resolvers.js
@@ -1,10 +1,28 @@
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const User = require("../models/user");
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
     const { email, name, password } = userInput;
+    const errors = [];
+
+    if (!validator.isEmail(email)) {
+      errors.push({ message: "Invalid email!" });
+    }
+
+    if (
+      validator.isEmpty(password) ||
+      !validator.isLength(password, { min: 5 })
+    ) {
+      errors.push({ message: "Invalid password!" });
+    }
+
+    if (errors.length > 0) {
+      throw new Error("Invalid input!");
+    }
+
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
