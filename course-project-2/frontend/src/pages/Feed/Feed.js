bff2984dfb509f17a2c6e2584032aa9ec74e4dc1
jaggerabney <jagger.abney@gmail.com>
2023-03-04 23:58:21 -0800

    post creation now works and uses graphql

--- course-project-2/frontend/src/pages/Feed/Feed.js
+++ course-project-2/frontend/src/pages/Feed/Feed.js
@@ -220,7 +220,17 @@ class Feed extends Component {
         };
 
         this.setState((prevState) => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              (p) => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
