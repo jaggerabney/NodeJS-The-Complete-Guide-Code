2104101e4a161220ad54b74bac2205a9019e206e
jaggerabney <jagger.abney@gmail.com>
2023-03-14 21:39:21 -0700

    added test utilizing jwt via a mock

--- project-5/test/auth-middleware.js
+++ project-5/test/auth-middleware.js
@@ -1,4 +1,5 @@
 const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = require("../middleware/is-auth");
 
@@ -23,5 +24,33 @@ describe("Auth middleware", function () {
     };
 
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should throw an error if the Auth token cannot be verified", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyz";
+      },
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId after decoding the token", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer poiwenrgiwoneroignwergo";
+      },
+    };
+
+    jwt.verify = function () {
+      return {
+        userId: "abc",
+      };
+    };
+
+    authMiddleware(req, {}, () => {});
+
+    expect(req).to.have.property("userId");
   });
 });
