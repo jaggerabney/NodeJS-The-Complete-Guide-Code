84f9b48363a3d2e489d5aa6f94ebca2466d42919
jaggerabney <jagger.abney@gmail.com>
2023-03-14 21:18:10 -0700

    organized tests using describe

--- project-5/test/auth-middleware.js
+++ project-5/test/auth-middleware.js
@@ -2,14 +2,26 @@ const expect = require("chai").expect;
 
 const authMiddleware = require("../middleware/is-auth");
 
-it("should throw an error if no Auth header is present", function () {
-  const req = {
-    get: function (headerName) {
-      return null;
-    },
-  };
+describe("Auth middleware", function () {
+  it("should throw an error if no Auth header is present", function () {
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
 
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    "Not authenticated."
-  );
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if the Auth header value is only one word", function () {
+    const req = {
+      get: function (headerName) {
+        return "xyz";
+      },
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
--- project-5/test/start.js
+++ project-5/test/start.js
@@ -1,15 +1,17 @@
 const expect = require("chai").expect;
 
-it("should add numbers correctly", function () {
-  const num1 = 2;
-  const num2 = 3;
+describe("Intro tests", function () {
+  it("should add numbers correctly", function () {
+    const num1 = 2;
+    const num2 = 3;
 
-  expect(num1 + num2).to.equal(5);
-});
+    expect(num1 + num2).to.equal(5);
+  });
 
-it("shouldn't give a result of six", function () {
-  const num1 = 2;
-  const num2 = 3;
+  it("shouldn't give a result of six", function () {
+    const num1 = 2;
+    const num2 = 3;
 
-  expect(num1 + num2).not.to.equal(6);
+    expect(num1 + num2).not.to.equal(6);
+  });
 });
