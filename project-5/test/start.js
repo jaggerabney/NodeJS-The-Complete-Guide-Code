9da4a4d514691a20153566fb7b99895d75131c02
jaggerabney <jagger.abney@gmail.com>
2023-03-14 21:00:51 -0700

    added: base code for project 5, mocha, chai, tests

---
+++ project-5/test/start.js
@@ -0,0 +1,15 @@
+const expect = require("chai").expect;
+
+it("should add numbers correctly", function () {
+  const num1 = 2;
+  const num2 = 3;
+
+  expect(num1 + num2).to.equal(5);
+});
+
+it("shouldn't give a result of six", function () {
+  const num1 = 2;
+  const num2 = 3;
+
+  expect(num1 + num2).not.to.equal(6);
+});
