9d3d73c397dbddfd8780af1163a4e614d54b5307
jaggerabney <jagger.abney@gmail.com>
2023-04-10 21:05:06 -0700

    explored using promise-based NodeJS modules

--- project-6/response-handler.js
+++ project-6/response-handler.js
@@ -1,10 +1,15 @@
-// const fs = require("fs");
-import fs from "fs";
+// const fs = require("fs").promises;
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const resHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf8", (err, data) => {
-    res.send(data);
-  });
+  fs.readFile("my-page.html", "utf8")
+    .then((data) => res.send(data))
+    .catch((error) => console.log(error));
 };
 
 export default resHandler;
