dcc728b4366677b8157fca54ba92c61e2bd9874b
jaggerabney <jagger.abney@gmail.com>
2023-04-11 22:18:19 -0700

    modified add to utilize union types

--- project-7/app.ts
+++ project-7/app.ts
@@ -1,9 +1,15 @@
 const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
-const buttonElement = document.querySelector("button") as HTMLButtonElement;
+const buttonElement = document.querySelector("button")!;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number | string, num2: number | string) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return num1 + " " + num2;
+  } else {
+    return +num1 + +num2;
+  }
 }
 
 buttonElement.addEventListener("click", () => {
