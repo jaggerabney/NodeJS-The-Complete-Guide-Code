8d326cb4dd72674741e63e02bffb8ecdc67b0f92
jaggerabney <jagger.abney@gmail.com>
2023-04-15 18:52:58 -0700

    implemented update and delete operations

--- project-8/routes/todos.ts
+++ project-8/routes/todos.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { Todo } from "../models/todo";
 
 // Dummy values
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -18,6 +18,27 @@ router.post("/todo", (req, res, next) => {
   };
 
   todos.push(newTodo);
+
+  res.status(201).json({ message: "Added todo!", todo: newTodo, todos });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex >= 0) {
+    todos[todoIndex].text = req.body.text;
+
+    res.status(200).json({ message: "Updated todo!", todos });
+  } else {
+    res.status(404).json({ message: "Couldn't find todo for the given ID!" });
+  }
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  todos = todos.filter((todo) => todo.id !== req.params.todoId);
+
+  res.status(200).json({ message: "Deleted todo!", todos });
 });
 
 export default router;
