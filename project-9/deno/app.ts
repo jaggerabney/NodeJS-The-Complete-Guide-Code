96ee61068ae6cc21d67184ee6530ad648ba8363a
jaggerabney <jagger.abney@gmail.com>
2023-04-16 15:37:56 -0700

    built simple todo API using deno and oak

--- project-9/deno/app.ts
+++ project-9/deno/app.ts
@@ -1,9 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
+import todoRoutes from "./routes/todos.ts";
+
 const app = new Application();
 
-app.use((context) => {
-  context.response.body = "Hello world!";
-});
+app.use(todoRoutes.routes());
+app.use(todoRoutes.allowedMethods());
 
 await app.listen({ port: 3000 });
---
+++ project-9/deno/routes/todos.ts
@@ -0,0 +1,58 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+
+interface Todo {
+  id: string;
+  text: string;
+}
+
+let todos: Todo[] = [];
+
+router.get("/todos", (context) => {
+  context.response.body = {
+    message: "Fetched todos!",
+    todos
+  };
+});
+
+router.post("/todos", async (context) => {
+  const data = await context.request.body().value;
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: data.text
+  };
+
+  todos.push(newTodo);
+
+  context.response.body = {
+    message: "Created todo!",
+    todos
+  };
+});
+
+router.put("/todos/:todoId", async (context) => {
+  const todoId = context.params.todoId;
+  const targetIndex = todos.findIndex((todo) => todo.id === todoId);
+  const data = await context.request.body().value;
+
+  todos[targetIndex].text = data.text;
+
+  context.response.body = {
+    message: "Updated todo!",
+    todos
+  };
+});
+
+router.delete("/todos/:todoId", (context) => {
+  const todoId = context.params.todoId;
+
+  todos = todos.filter((todo) => todo.id !== todoId);
+
+  context.response.body = {
+    message: "Deleted todo!",
+    todos
+  };
+});
+
+export default router;
