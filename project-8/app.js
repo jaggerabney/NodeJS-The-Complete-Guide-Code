7c3f1581117025ab590710fd6a2696e63139aa70
jaggerabney <jagger.abney@gmail.com>
2023-04-15 18:33:37 -0700

    installed @types for node/express

--- project-8/app.js
+++ project-8/app.js
@@ -1,5 +1,8 @@
-const express = require("express");
-
-const app = express();
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 app.listen(3000);
