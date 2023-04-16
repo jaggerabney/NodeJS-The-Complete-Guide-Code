258e13c9e59e2bf281d6ec6084a577d35b5baf4c
jaggerabney <jagger.abney@gmail.com>
2023-04-16 14:44:57 -0700

    converted app.ts to basic web server

--- project-9/app.ts
+++ project-9/app.ts
@@ -1,7 +1,16 @@
-const text = "This is only a test!";
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
+const server = Deno.listen({ port: 3000 });
+console.log("Server running at http://localhost:3000/.");
 
-Deno.writeFile("message_deno.txt", data).then(() =>
-  console.log("Wrote to message.txt!")
-);
+for await (const conn of server) {
+  serveHttp(conn);
+}
+
+async function serveHttp(conn: Deno.Conn) {
+  const httpConnection = Deno.serveHttp(conn);
+
+  for await (const requestEvent of httpConnection) {
+    const body = "Hello world!";
+
+    requestEvent.respondWith(new Response(body, { status: 200 }));
+  }
+}
