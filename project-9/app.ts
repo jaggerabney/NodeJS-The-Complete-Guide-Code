db159fe3c634031814509e061c93a07b4fda9e66
jaggerabney <jagger.abney@gmail.com>
2023-04-16 14:58:27 -0700

    explored oak, AKA express for deno

--- project-9/app.ts
+++ project-9/app.ts
@@ -1,16 +1,9 @@
-const server = Deno.listen({ port: 3000 });
-console.log("Server running at http://localhost:3000/.");
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-for await (const conn of server) {
-  serveHttp(conn);
-}
+const app = new Application();
 
-async function serveHttp(conn: Deno.Conn) {
-  const httpConnection = Deno.serveHttp(conn);
+app.use((context) => {
+  context.response.body = "Hello world!";
+});
 
-  for await (const requestEvent of httpConnection) {
-    const body = "Hello world!";
-
-    requestEvent.respondWith(new Response(body, { status: 200 }));
-  }
-}
+await app.listen({ port: 3000 });
